8967b619082b28d29348f017c1320ccaf32154fa
Valbort Valveline <valbort@ro.ru>
2020-05-13 21:14:00 +0300

    test

--- testwebhook.py
+++ testwebhook.py
@@ -1,10 +1,8 @@
 import telebot
 
-updater.start_webhook ( listen = ' 0.0.0.0 ' ,
+telebot.TeleBot.update_listener( listen = ' 0.0.0.0 ' ,
                        port = 8443 ,
                        url_path = ' 534247055:AAGkXwOFST2ATDcKqNF8g-aT0r9gY-yjgyE ' ,
                        key = ' cert/private.key ' ,
                        cert = ' cert/cert.pem ' ,
                        webhook_url = ' https://vlbtest.site:8443 / TOKEN ' )
-
-

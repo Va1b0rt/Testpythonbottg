fb423a3280863b2ea59dd86e102a95c9b110c74a
Valbort Valveline <valbort@ro.ru>
2020-05-13 21:55:30 +0300

    Добавлены новые сертефикаты и обновлен вебхук

--- testwebhook.py
+++ testwebhook.py
@@ -1,8 +1,26 @@
-import telebot
+from flask import Flask, request, jsonify
+import telebot, time
 
-telebot.TeleBot.update_listener( listen = ' 0.0.0.0 ' ,
-                       port = 8443 ,
-                       url_path = ' 534247055:AAGkXwOFST2ATDcKqNF8g-aT0r9gY-yjgyE ' ,
-                       key = ' cert/private.key ' ,
-                       cert = ' cert/cert.pem ' ,
-                       webhook_url = ' https://vlbtest.site:8443 / TOKEN ' )
+server = Flask(name)
+bot = telebot.TeleBot(bot_token)
+bot.remove_webhook()
+time.sleep(2)
+bot.set_webhook(url="https://185.43.5.36:8443", certificate=open('/Testpythonbottg/cert/rootCA.crt', 'r'))
+
+@server.route("/", methods=['POST'])
+def getMessage():
+  r = request.get_json()
+  if "message" in r.keys():
+    chat_id = r["message"]["chat"]["id"]
+    if "text" in r["message"]:
+      text_mess = r["message"]["text"]
+    else:
+      bot.send_message(chat_id=chat_id, text="Какая то не понятная проблема", parse_mode='HTML')
+      return "ok", 200
+
+  if text_mess == '/start':
+    bot.send_message(chat_id=chat_id, text="Привет WebHook")
+    return "ok", 200
+
+if name == "main":
+  server.run(host="IP", port=int(os.environ.get('PORT', '443')), ssl_context=('/Testpythonbottg/cert/rootCA.crt', '/Testpythonbottg/cert/rootCA.key'))
